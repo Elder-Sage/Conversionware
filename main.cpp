c93ea8a2801dc952acce9ffe6609e925295556f4
Clint <masterplinko@gmail.com>
2014-10-16 15:09:40 -0700

    Main changes
    fixing spacing and tab lengths to clean up the code

--- main.cpp
+++ main.cpp
@@ -8,47 +8,47 @@ void binary(int start){
     int bin[100] = {0};  //place holder for binary list
     int counter = 0;     //keeps track of how long the array is
 
-	while(start >= 1){
-    rem = start % 2;        
-    if(rem == 0){           //Modular Division used to find remainder
-      bin[counter] = 0;     //check if input is 0 place 0 in array
-    }                       //if remainder is anything else place 1 in array
-    else{
-    bin[counter] = 1;
-    }
+    while(start >= 1){
+    	rem = start % 2;        
+    	if(rem == 0){           //Modular Division used to find remainder
+      		bin[counter] = 0;   //check if input is 0 place 0 in array
+    	}                       //if remainder is anything else place 1 in array
+    	else{
+			bin[counter] = 1;
+    	}
     counter++;
     start /= 2;
-  }
-  counter--; //counter incrimented 1 uneaded time in above loop so this fixes that so printing works correctly
-  cout << "The inputed number in Binary is: ";
-  while(counter >= 0){
-    cout << bin[counter]; //prints binary string 
-    counter--;
-  }
-  cout << endl;
+    }
+    counter--;		//counter incrimenented 1 more time then needed in loop
+    cout << "The inputed number in Binary is: ";
+    while(counter >= 0){
+    	cout << bin[counter]; //prints binary string 
+    	counter--;
+    }
+    cout << endl;
 }
 	
 
 int main(){
-  char repeat = 'n';
-  int number = 0;
+	char repeat = 'n';
+  	int number = 0;
   
-  cout << "Enter a number in Base 10 notation: ";
-  cin >> number;
-  binary(number);
-  cout << endl;
+  	cout << "Enter a number in Base 10 notation: ";
+  	cin >> number;
+  	binary(number);
+  	cout << endl;
 
-  cout << "Convert another number enter Y or N: ";		
-  cin >> repeat;
-  if(repeat == 'y' || repeat == 'Y'){
-	  cout << endl;
-	  main();
-  }
-  else if(repeat == 'n' || repeat == 'N'){
-	  return 0;
-  }
-  else{
-	  cout << "Error closing program" << endl;			
-  }
-  return 0;
-  }
+  	cout << "Convert another number enter Y or N: ";		
+  	cin >> repeat;
+  	if(repeat == 'y' || repeat == 'Y'){
+		cout << endl;
+	  	main();
+  	}
+  	else if(repeat == 'n' || repeat == 'N'){
+		return 0;
+  	}
+  	else{
+	  	cout << "Error closing program" << endl;			
+  	}
+  	return 0;
+}
