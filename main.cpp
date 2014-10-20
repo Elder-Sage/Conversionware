0fd4432d5ca303c93c94f65daa09f536686b0ca7
Clint <masterplinko@gmail.com>
2014-10-19 21:12:44 -0700

    adding more options and organizing switch

--- main.cpp
+++ main.cpp
@@ -6,14 +6,12 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void dectobinary(){
-	int start = 0;
+void dectobinary(int start){
 	int rem = 0;         //holds remainder
 	int bin[100] = { 0 };  //place holder for binary list
 	int counter = 0;     //keeps track of how long the array is
 
-	cout << "Enter a numer in Base 10 notation: ";
-	cin >> start;
+
 	while (start >= 1){
 		rem = start % 2;
 		if (rem == 0){           	//Modular Division used to find remainder
@@ -34,15 +32,10 @@ void dectobinary(){
 	cout << endl << endl;
 }
 
-void bintodecimal(){
-	std::string bin;
+int bintodecimal(std::string bin){
 	int converted = 0;
 	int expo;
 	int loop;
-
-	cout << "I will convert a binary number to a standard Base 10 number." << endl;
-	cout << "Enter a binary number: ";
-	cin >> bin;
 	expo = bin.length() - 1;
 	loop = expo;
 	for (int x = 0; x <= loop; x++){
@@ -54,20 +47,21 @@ void bintodecimal(){
 			expo--;
 		}
 	}
-	cout << "Once converted your input is " << converted << " in Base 10 notation" << endl;
-
+	return converted;
 }
 
 int main(){
 	int menu;
 	char answer = 'n';
 	int input;
+	int converted;
+	std::string bin;
 	do{
 		cout << "Choose" << endl;
-		cout << "1: Binary -> Decimal" << endl;
-		cout << "2: Decimal -> Binary" << endl;
-		cout << "3: Decimal -> Octal" << endl;
-		cout << "4: Decimal -> Hexideximal" << endl;
+		cout << "1: Decimal -> Binary" << endl;
+		cout << "2: Decimal -> Octal" << endl;
+		cout << "3: Decimal -> Hexideximal" << endl;
+		cout << "4: Binary -> Decimal" << endl;
 		cout << "5: Binary -> Octal" << endl;
 		cout << "6: Binary -> Hexidecimal" << endl;
 		cout << "Choice: ";
@@ -75,23 +69,48 @@ int main(){
 		switch (menu){
 		case 1:
 			cout << endl;
-			bintodecimal();
+			cout << "Enter a numer in Base 10 notation: ";
+			cin >> input;
+			dectobinary(input);
 			break;
-		case 2:
+		case 2: 
 			cout << endl;
-			dectobinary();
-			break;
-		case 3: 
 			cout << "Enter a Base 10 number: ";
 			cin >> input;
 			cout << "The input in Octal is " << std::oct << input << endl;
 			break;
-		case 4:
+		case 3:
+			cout << endl;
 			cout << "Enter a Base 10 number: ";
 			cin >> input;
 			cout << "The input in Hexidecimal is " << std::hex << input << endl;
 			break;
-		}
+		case 4:
+			cout << endl;
+			cout << "I will convert a binary number to a standard Base 10 number." << endl;
+			cout << "Enter a binary number: ";
+			cin >> bin;
+			converted = bintodecimal(bin);
+			cout << "Once converted your input is " << converted << " in Base 10 notation" << endl;
+			break;
+		case 5:
+			cout << endl;
+			cout << "I will convert a binary number to a standard Base 10 number." << endl;
+			cout << "Enter a binary number: ";
+			cin >> bin;
+			converted = bintodecimal(bin);
+			cout << "Once converted your input is " << std::oct << converted << " in Octal." << endl;
+			break;
+		case 6:
+			cout << endl;
+			cout << "I will convert a binary number to a standard Base 10 number." << endl;
+			cout << "Enter a binary number: ";
+			cin >> bin;
+			converted = bintodecimal(bin);
+			cout << "Once converted your input is " << std::hex << converted << " in Hexadecimal." << endl;
+			break;
+
+		} //end of switch
 		cout << "Would you like to run the program again Press Y or N: ";
 		cin >> answer;
 		cout << endl;
